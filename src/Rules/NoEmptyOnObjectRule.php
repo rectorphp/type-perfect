1293e3a691b88e0003cc78496bd8bd0e35bdc5f9
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-06-18 19:42:38 +0700

    [Rules] Skip Property fetch on NoIssetOnObjectRule (#24)

--- src/Rules/NoEmptyOnObjectRule.php
+++ src/Rules/NoEmptyOnObjectRule.php
@@ -5,23 +5,25 @@ declare(strict_types=1);
 namespace Rector\TypePerfect\Rules;
 
 use PhpParser\Node;
-use PhpParser\Node\Expr;
-use PhpParser\Node\Expr\ArrayDimFetch;
 use PhpParser\Node\Expr\Empty_;
-use PhpParser\Node\Expr\Variable;
 use PHPStan\Analyser\Scope;
 use PHPStan\Rules\Rule;
-use PHPStan\Type\TypeCombinator;
+use Rector\TypePerfect\Guard\EmptyIssetGuard;
 
 /**
  * @implements Rule<Empty_>
  */
-final class NoEmptyOnObjectRule implements Rule
+final readonly class NoEmptyOnObjectRule implements Rule
 {
     /**
      * @var string
      */
     public const ERROR_MESSAGE = 'Use instanceof instead of empty() on object';
+
+    public function __construct(
+        private EmptyIssetGuard $emptyIssetGuard
+    ) {
+    }
 
     public function getNodeType(): string
     {
@@ -34,34 +36,12 @@ final class NoEmptyOnObjectRule implements Rule
      */
     public function processNode(Node $node, Scope $scope): array
     {
-        $expr = $node->expr;
-        if ($this->shouldSkipVariable($expr, $scope)) {
+        if ($this->emptyIssetGuard->isLegal($node->expr, $scope)) {
             return [];
         }
 
         return [
             self::ERROR_MESSAGE,
         ];
-    }
-
-    private function shouldSkipVariable(Expr $expr, Scope $scope): bool
-    {
-        if ($expr instanceof ArrayDimFetch) {
-            return true;
-        }
-
-        if ($expr instanceof Variable) {
-            if ($expr->name instanceof Expr) {
-                return true;
-            }
-
-            if (! $scope->hasVariableType($expr->name)->yes()) {
-                return true;
-            }
-        }
-
-        $varType = $scope->getType($expr);
-        $varType = TypeCombinator::removeNull($varType);
-        return $varType->getObjectClassNames() === [];
     }
 }
