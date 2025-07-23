da26baf003e0a6d0bab00563e67508eacc592da3
Markus Staab <markus.staab@redaxo.de>
2025-07-23 14:31:49 +0200

    remove unused import

--- tests/Rules/NarrowPrivateClassMethodParamTypeRule/Fixture/SkipVariadics.php
+++ tests/Rules/NarrowPrivateClassMethodParamTypeRule/Fixture/SkipVariadics.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Rector\TypePerfect\Tests\Rules\NarrowPrivateClassMethodParamTypeRule\Fixture;
 
 use PhpParser\Node;
-use PhpParser\Node\Expr\MethodCall;
 
 class SkipVariadics
 {
